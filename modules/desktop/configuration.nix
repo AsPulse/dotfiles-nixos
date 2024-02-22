fe366cb65d33cbb5699fd4c154a6b21067e2a0c1
AsPulse <contact@aspulse.dev>
2024-02-22 07:51:21 +0000

    Add user config

--- modules/desktop/configuration.nix
+++ modules/desktop/configuration.nix
@@ -1,4 +1,34 @@
-{ ... }: {
+{ pkgs, ... }: {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   system.stateVersion = "23.11";
+
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    wget
+    curl
+  ];
+  environment.variables.EDITOR = "vim";
+
+  users.users.aspulse = {
+    shell = pkgs.zsh;
+    createHome = true;
+    home = "/home/aspulse";
+    extraGroups = [ "wheel" ];
+    isNormalUser = true;
+    uid = 1000;
+  };
+
+  programs = {
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+    };
+    zsh = {
+      enable = true;
+    };
+    starship = {
+      enable = true;
+    };
+  };
 }
