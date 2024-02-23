b7d43cec4ecdea919c17b710d26b06c4d4888b00
AsPulse / あすぱる <contact@aspulse.dev>
2024-02-23 07:18:08 +0000

    feat: Setup gh-poi (#8)

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:AsPulse/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
