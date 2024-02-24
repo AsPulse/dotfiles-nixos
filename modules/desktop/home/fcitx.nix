33bdea190a047c9d493b4f78e73f900761809598
AsPulse / あすぱる <contact@aspulse.dev>
2024-02-24 10:31:19 +0900

    add vivaldi, hyprland support

--- modules/desktop/home/fcitx.nix
+++ modules/desktop/home/fcitx.nix
@@ -3,7 +3,13 @@
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-gtk
+      fcitx5-mozc
+      fcitx5-skk-qt
       fcitx5-configtool
     ];
   };
+
+  home.packages = with pkgs; [
+    libskk
+  ];
 }
