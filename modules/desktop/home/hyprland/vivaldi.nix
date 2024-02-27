4ddcd8d144e74d931f47ce2434e79e6f764163ae
AsPulse / あすぱる <contact@aspulse.dev>
2024-02-27 21:18:51 +0900

    Add vivaldi args

--- modules/desktop/home/hyprland/vivaldi.nix
+++ modules/desktop/home/hyprland/vivaldi.nix
@@ -5,6 +5,16 @@
         "--enable-features=UseOzonePlatform"
         "--ozone-platform=wayland"
         "--gtk-version=4"
+        "--enable-features=VaapiVideoEncoder,VaapiVideoDecoder,CanvasOopRasterization,UseSkiaRenderer"
+        "--enable-gpu-rasterization"
+        "--enable-skia-renderer"
+        "--canvas-oop-rasterization"
+        "--disable-gpu-driver-bug-workarounds"
+        "--enable-zero-copy"
+        "--ignore-gpu-blocklist"
+        "--use-vulkan"
+        "--enable-quic"
+        "--enable-tls13-early-data"
       ];
       proprietaryCodecs = true;
       enableWidevine = false;
