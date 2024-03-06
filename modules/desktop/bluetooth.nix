e1edc7d481876c0a5c5bebca0c7739fecfd78d2e
AsPulse / あすぱる <contact@aspulse.dev>
2024-03-06 19:25:11 +0900

    Enable sink

--- modules/desktop/bluetooth.nix
+++ modules/desktop/bluetooth.nix
@@ -1,4 +1,15 @@
 { ... }: {
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = { 
+      General = {
+        Enable="Source,Sink,Media,Socket";
+        Experimental="true";
+      };
+      Policy = {
+        AutoEnable = true;
+      };
+    };
+  };
 }
