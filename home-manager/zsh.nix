eac4f358cb37b1b27873408ff7e416850ebb89fa
AsPulse / あすぱる <contact@aspulse.dev>
2024-03-31 12:22:19 +0900

    feat: add zsh alias

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -12,6 +12,7 @@
       ls = "eza --icons --classify";
       la = "eza --all --icons --classify";
       ll = "eza --long --all --git --icons";
+      docker-build-multi = "sudo docker buildx build --platform linux/amd64,linux/arm64";
     };
     profileExtra = "export PATH=\"\${KREW_ROOT:-$HOME/.krew}/bin:$PATH";
   };
